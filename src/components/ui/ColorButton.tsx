597b62a35ea3ccd5b0e75bf1df19466277a65bef
pyeong777 <seowns9520@gmail.com>
2023-06-15 05:26:38 +0900

    design: 버튼 컴포넌트 props & css 추가

--- src/components/ui/ColorButton.tsx
+++ src/components/ui/ColorButton.tsx
@@ -1,13 +1,18 @@
 type Props = {
   text: string;
   onClick: () => void;
+  size: "small" | "big";
 };
 
-export default function ColorButton({ text, onClick }: Props) {
+export default function ColorButton({ text, onClick, size = "small" }: Props) {
   return (
-    <div className="p-[0.15rem] rounded-md bg-gradient-to-bl from-emerald-400 to-amber-300">
+    <div
+      className={`rounded-md bg-gradient-to-bl from-emerald-400 to-amber-300
+    ${size === "big" ? "p-[0.3rem]" : "p-[0.15rem] "}`}
+    >
       <button
-        className="text-base bg-white rounded-sm p-[0.3rem] hover:opacity-80 transition-opacity"
+        className={`bg-white rounded-sm  hover:opacity-80 transition-opacity
+        ${size === "big" ? "p-4 text-2xl" : "p-[0.3rem] text-base "}`}
         onClick={onClick}
       >
         {text}
