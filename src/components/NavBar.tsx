97cd67a101b19aa981ac03f654eb8a55a3903341
pyeong777 <seowns9520@gmail.com>
2023-06-14 18:07:24 +0900

    feat: NavBar 컴포넌트 추가

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import HomeIcon from "./ui/icons/HomeIcon";
+import HomeFillIcon from "./ui/icons/HomeFillIcon";
+import SearchIcon from "./ui/icons/SearchIcon";
+import SearchFillIcon from "./ui/icons/SearchFillIcon";
+import NewIcon from "./ui/icons/NewIcon";
+import NewFillIcon from "./ui/icons/NewFillIcon";
+import { usePathname } from "next/navigation";
+import ColorButton from "./ui/ColorButton";
+
+const menu = [
+  {
+    href: "/",
+    icon: <HomeIcon />,
+    clickedIcon: <HomeFillIcon />,
+  },
+  {
+    href: "/search",
+    icon: <SearchIcon />,
+    clickedIcon: <SearchFillIcon />,
+  },
+  {
+    href: "/new",
+    icon: <NewIcon />,
+    clickedIcon: <NewFillIcon />,
+  },
+];
+
+export default function NavBar() {
+  const pathName = usePathname();
+  return (
+    <div className="flex items-center justify-between max-w-screen-xl px-6 mx-auto">
+      <Link href="/">
+        <h1 className="flex text-2xl font-bold leading-loose">
+          <Image
+            className="mr-2"
+            src="/assets/logo.svg"
+            alt="logo"
+            width="36"
+            height="36"
+          />
+          Valyrian
+        </h1>
+      </Link>
+      <nav>
+        <ul className="flex items-center gap-4 p-4">
+          {menu.map((item) => (
+            <li key={item.href}>
+              <Link href={item.href}>
+                {pathName === item.href ? item.clickedIcon : item.icon}
+              </Link>
+            </li>
+          ))}
+          <ColorButton text="Sign in" onClick={() => {}} />
+        </ul>
+      </nav>
+    </div>
+  );
+}
