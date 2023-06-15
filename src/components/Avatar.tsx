d7999cd9361d5ef444f58a481cfb87f28df0b61a
pyeong777 <seowns9520@gmail.com>
2023-06-16 08:34:49 +0900

    feat: avatar 컴포넌트 css  & sidebar 컴포넌트 추가

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,17 +1,39 @@
 type Props = {
   image?: string | null;
+  size?: "small" | "normal";
+  highlight?: boolean;
 };
 
-export default function Avatar({ image }: Props) {
+export default function Avatar({
+  image,
+  size = "normal",
+  highlight = false,
+}: Props) {
   return (
-    <div className="rounded-full bg-gradient-to-bl from-emerald-400 to-amber-300 w-9 h-9">
+    <div className={getContainerStyle(size, highlight)}>
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
-        className="rounded-full p-[0.1rem]"
+        className={`bg-white rounded-full p-[0.1rem] ${getImageSizeStyle(
+          size
+        )}`}
         alt="user profile"
         src={image ?? undefined}
         referrerPolicy="no-referrer"
       />
     </div>
   );
+}
+
+function getContainerStyle(size: string, highlight: boolean): string {
+  const baseStyle = "rounded-full flex justify-center items-center";
+  const highlightStyle = highlight
+    ? "bg-gradient-to-bl from-emerald-400 to-amber-300"
+    : "";
+  const sizeStyle = size === "small" ? "w-9 h-9" : "w-[60px] h-[60px]";
+
+  return `${baseStyle} ${highlightStyle} ${sizeStyle}`;
+}
+
+function getImageSizeStyle(size: string): string {
+  return size === "small" ? "w-[34px] h-[34px] p-[0.1rem]" : "w-16 h-16";
 }
---
+++ src/components/SideBar.tsx
@@ -0,0 +1,26 @@
+import { User } from "@/model/user";
+import Avatar from "./Avatar";
+
+type Props = {
+  user: User;
+};
+
+export default function SideBar({ user: { name, username, image } }: Props) {
+  return (
+    <>
+      <div className="flex itmes-center">
+        {image && <Avatar image={image} />}
+        <div className="my-auto ml-4">
+          <p className="font-bold">{username}</p>
+          <p className="text-neutral-500">{name}</p>
+        </div>
+      </div>
+      <p className="mt-8 text-sm text-neutral-500">
+        소개 • 도움말 • API • 개인정보처리방침 • 약관 • 위치 • 언어
+      </p>
+      <p className="mt-8 text-sm text-neutral-500">
+        @2023 Mangoloco from pyeong777
+      </p>
+    </>
+  );
+}
