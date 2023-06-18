8d647fb7021633b8613f3f3572cd4a454f819dd3
pyeong777 <seowns9520@gmail.com>
2023-06-19 08:13:04 +0900

    feat: 팔로잉 바 컴포넌트 추가

---
+++ src/components/FollowingBar.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { DetailUser } from "@/model/user";
+import Link from "next/link";
+import { PropagateLoader } from "react-spinners";
+import useSWR from "swr";
+import Avatar from "./Avatar";
+import Scrollbar from "./ui/Scrollbar";
+
+export default function FollowingBar() {
+  const { data, isLoading, error } = useSWR<DetailUser>("/api/me");
+  // const users = data?.following;
+  const users = data?.following && [
+    ...data?.following,
+    ...data?.following,
+    ...data?.following,
+  ];
+
+  return (
+    <section className="flex items-center justify-center w-full p-4 mb-4 rounded-lg shadow-sm shadow-neutral-300 min-h-[90px] overflow-x-auto">
+      {isLoading ? (
+        <PropagateLoader size={8} color="green" />
+      ) : (
+        (!users || users.length === 0) && <p>{`팔로잉 유저가 없습니다`}</p>
+      )}
+      {users && users.length > 0 && (
+        <Scrollbar>
+          {users.map((user) => (
+            <Link
+              key={user.username}
+              className="flex flex-col items-center w-20"
+              href={`/user/${user.username}`}
+            >
+              <Avatar image={user.image} highlight />
+              <p className="w-full overflow-hidden text-sm text-center text-ellipsis">
+                {user.username}
+              </p>
+            </Link>
+          ))}
+        </Scrollbar>
+      )}
+    </section>
+  );
+}
