526d5e33526b8b45dcc3fc65513e5239bdb75d51
pyeong777 <seowns9520@gmail.com>
2023-06-19 08:12:42 +0900

    feat: main page 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
-export default function Home() {
-  return <h1 className="text-gray-600">hello</h1>;
+import FollowingBar from "@/components/FollowingBar";
+import PostList from "@/components/PostList";
+import SideBar from "@/components/SideBar";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+  if (!user) {
+    redirect("/signin");
+  }
+  return (
+    <section className="w-full flex flex-col sm:flex-row max-w-[850px] p-4 mx-auto">
+      <div className="w-full min-w-0 basis-3/4">
+        <FollowingBar />
+        <PostList />
+      </div>
+      <div className="sm:ml-8 basis-1/4">
+        <SideBar user={user} />
+      </div>
+    </section>
+  );
 }
