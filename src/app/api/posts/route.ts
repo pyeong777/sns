0ba821758a479fb3259fed39f2b02658a97cb73b
pyeong777 <seowns9520@gmail.com>
2023-06-25 07:27:30 +0900

    feat: post GET api 추가

---
+++ src/app/api/posts/route.ts
@@ -0,0 +1,16 @@
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+import { authOptions } from "../auth/[...nextauth]/route";
+import { getFollowingPostOf } from "@/service/post";
+
+export async function GET() {
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+
+  if (!user) {
+    return new Response("Authentication Error", { status: 401 });
+  }
+  return getFollowingPostOf(user.username).then((data) =>
+    NextResponse.json(data)
+  );
+}
