5302e7bb2168d9ef07c73482220bbba2d62633cc
pyeong777 <seowns9520@gmail.com>
2023-07-06 02:22:46 +0900

    feat: postlist 컴포넌트 추가

---
+++ src/components/PostList.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { SimplePost } from "@/model/post";
+import { GridLoader } from "react-spinners";
+import useSWR from "swr";
+import PostListCard from "./PostListCard";
+
+export default function PostList() {
+  const { data, isLoading } = useSWR<SimplePost[]>("/api/posts");
+  return (
+    <section>
+      {isLoading && (
+        <div className="mt-32 text-center">
+          <GridLoader color="yellow" />
+        </div>
+      )}
+      {data && (
+        <ul>
+          {data.map((post) => (
+            <li key={post.id} className="mb-4">
+              <PostListCard post={post} />
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
+  );
+}
