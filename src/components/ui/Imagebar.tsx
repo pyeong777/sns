68b5396edb5694f107f60d5981239ef8a5ef9dcc
pyeong777 <seowns9520@gmail.com>
2023-07-06 02:22:55 +0900

    feat: 이미지 슬라이더 추가

---
+++ src/components/ui/Imagebar.tsx
@@ -0,0 +1,38 @@
+import { SimplePost } from "@/model/post";
+import Image from "next/image";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+const responsive = {
+  desk: {
+    breakpoint: { max: 4000, min: 576 },
+    items: 1,
+  },
+  mobile: {
+    breakpoint: { max: 576, min: 0 },
+    items: 1,
+  },
+};
+
+type Props = {
+  post: SimplePost;
+};
+
+export default function Imagebar({ post }: Props) {
+  const { username, images } = post;
+
+  return (
+    <Carousel responsive={responsive} showDots={true} arrows={false}>
+      {images.map((image, index) => (
+        <Image
+          className="object-cover w-full aspect-square"
+          key={index}
+          src={image}
+          alt={`photo by ${username}`}
+          width={500}
+          height={500}
+        />
+      ))}
+    </Carousel>
+  );
+}
