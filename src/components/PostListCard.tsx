14dca3d28155da2835f526f40ad7e7201b884375
pyeong777 <seowns9520@gmail.com>
2023-07-19 16:55:13 +0900

    refactor: Actionbar 컴포넌트 분리

--- src/components/PostListCard.tsx
+++ src/components/PostListCard.tsx
@@ -1,17 +1,16 @@
 import { SimplePost } from "@/model/post";
 import Avatar from "./Avatar";
-import HeartIcon from "./ui/icons/HeartIcon";
-import BookMarkIcon from "./ui/icons/BookMarkIcon";
-import { parseDate } from "@/util/date";
 import Imagebar from "./ui/Imagebar";
 import CommentForm from "./CommentForm";
+import Actionbar from "./Actionbar";
 
 type Props = {
   post: SimplePost;
+  priority?: boolean;
 };
 
-export default function PostListCard({ post }: Props) {
-  const { userImage, username, createdAt, likes, text } = post;
+export default function PostListCard({ post, priority = false }: Props) {
+  const { userImage, username, createdAt, likes, text, images } = post;
   return (
     <article className="border border-gray-200 rounded-lg shadow-md">
       <div className="flex items-center p-2">
@@ -19,27 +18,15 @@ export default function PostListCard({ post }: Props) {
         <span className="ml-2 font-bold text-gray-900">{username}</span>
       </div>
 
-      <Imagebar post={post} />
+      <Imagebar images={images} username={username} priority={priority} />
 
-      <div className="flex justify-between px-4 my-2">
-        <HeartIcon />
-        <BookMarkIcon />
-      </div>
-      <div className="px-4 py-1">
-        <p className="mb-2 text-sm font-bold sm:text-base">{`좋아요 ${
-          likes?.length ?? 0
-        }개`}</p>
-        <p>
-          <span className="mr-1 text-sm font-bold sm:text-base">
-            {username}
-          </span>
-          <span>{text}</span>
-        </p>
-        <p className="my-2 text-xs text-neutral-500">
-          {parseDate(createdAt, "ko")}
-        </p>
-        <CommentForm />
-      </div>
+      <Actionbar
+        likes={likes}
+        username={username}
+        text={text}
+        createdAt={createdAt}
+      />
+      <CommentForm />
     </article>
   );
 }
