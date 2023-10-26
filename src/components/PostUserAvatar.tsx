8c63dc927901bca00a5fc69196c5930ed73410fc
pyeong777 <seowns9520@gmail.com>
2023-10-26 14:02:46 +0900

    feat: PostUserAvatar 컴포넌트 추가

---
+++ src/components/PostUserAvatar.tsx
@@ -0,0 +1,15 @@
+import Avatar from "./Avatar";
+
+type Props = {
+  image: string;
+  username: string;
+};
+
+export default function PostUserAvatar({ image, username }: Props) {
+  return (
+    <div className="flex items-center p-2">
+      <Avatar image={image} size="medium" />
+      <span className="ml-2 font-bold text-gray-900">{username}</span>
+    </div>
+  );
+}
