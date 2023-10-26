9df4e7e6cc95148e145bcdd64101827ef9c733c2
pyeong777 <seowns9520@gmail.com>
2023-10-26 13:19:47 +0900

    feat: useDebounce 커스텀훅 적용(검색)

--- src/components/UserSearch.tsx
+++ src/components/UserSearch.tsx
@@ -5,17 +5,19 @@ import { FormEvent, useState } from "react";
 import useSWR from "swr";
 import GridSpinner from "./ui/GridSpinner";
 import UserCard from "./UserCard";
+import useDebounce from "@/hooks/debounce";
 
 export default function UserSearch() {
   // /api/search/${keyword}
   // 검색하는 keyword가 있다면 /api/search/bob -> 유저네임이나 네임에서 찾아서 리턴
   // 없을경우 /api/search 까지만 전송 -> 전체 유저의 배열을 리턴
   const [keyword, setKeyword] = useState("");
+  const debouncedKeyword = useDebounce(keyword);
   const {
     data: users,
     isLoading,
     error,
-  } = useSWR<UserSearchResult[]>(`/api/search/${keyword}`);
+  } = useSWR<UserSearchResult[]>(`/api/search/${debouncedKeyword}`);
 
   const onSubmit = (e: FormEvent) => {
     e.preventDefault();
