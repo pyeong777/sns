c33500d2eefa76fe813aef9124a2252836f8ac73
pyeong777 <seowns9520@gmail.com>
2023-10-26 13:13:31 +0900

    feat: UseSearch 컴포넌트 추가

---
+++ src/components/UserSearch.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { UserSearchResult } from "@/model/user";
+import { FormEvent, useState } from "react";
+import useSWR from "swr";
+import GridSpinner from "./ui/GridSpinner";
+import UserCard from "./UserCard";
+
+export default function UserSearch() {
+  // /api/search/${keyword}
+  // 검색하는 keyword가 있다면 /api/search/bob -> 유저네임이나 네임에서 찾아서 리턴
+  // 없을경우 /api/search 까지만 전송 -> 전체 유저의 배열을 리턴
+  const [keyword, setKeyword] = useState("");
+  const {
+    data: users,
+    isLoading,
+    error,
+  } = useSWR<UserSearchResult[]>(`/api/search/${keyword}`);
+
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <section className="flex flex-col items-center w-full max-w-xl my-4">
+      <form className="w-full mb-4" onSubmit={onSubmit}>
+        <input
+          className="w-full p-3 text-xl border border-gray-400 outline-none"
+          type="text"
+          autoFocus
+          placeholder="사용자의 이름을 검색하세요"
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
+        />
+      </form>
+      {error && <p>검색에 오류가 있습니다</p>}
+      {isLoading && <GridSpinner />}
+      {!isLoading && !error && users?.length === 0 && (
+        <p>찾는 사용자가 없습니다</p>
+      )}
+      <ul className="w-full p-4">
+        {users &&
+          users.map((user) => (
+            <li key={user.username}>
+              <UserCard user={user} />
+            </li>
+          ))}
+      </ul>
+    </section>
+  );
+}
