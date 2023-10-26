69032bb8c11d1a75fe66111416567e614c4785b3
pyeong777 <seowns9520@gmail.com>
2023-10-26 14:03:26 +0900

    feat: PostDetail, Avatar 컴포넌트 추가

--- src/components/PostListCard.tsx
+++ src/components/PostListCard.tsx
@@ -8,6 +8,8 @@ import { useState } from "react";
 import ModalPortal from "./ui/ModalPortal";
 import PostModal from "./PostModal";
 import Image from "next/image";
+import PostDetail from "./PostDetail";
+import PostUserAvatar from "./PostUserAvatar";
 
 type Props = {
   post: SimplePost;
@@ -19,11 +21,7 @@ export default function PostListCard({ post, priority = false }: Props) {
   const [openModal, setOpenModal] = useState(false);
   return (
     <article className="border border-gray-200 rounded-lg shadow-md">
-      <div className="flex items-center p-2">
-        <Avatar image={userImage} size="medium" />
-        <span className="ml-2 font-bold text-gray-900">{username}</span>
-      </div>
-
+      <PostUserAvatar image={userImage} username={username} />
       <Image
         className="object-cover w-full aspect-square"
         src={image}
@@ -44,7 +42,7 @@ export default function PostListCard({ post, priority = false }: Props) {
       {openModal && (
         <ModalPortal>
           <PostModal onClose={() => setOpenModal(false)}>
-            <p>포스트 상세 페이지</p>
+            <PostDetail post={post} />
           </PostModal>
         </ModalPortal>
       )}
