ec6ce6160897581be3275a1f8a998eac1d51c651
pyeong777 <seowns9520@gmail.com>
2023-10-30 15:31:13 +0900

    feat: 유저 상세 페이지 추가

---
+++ src/app/user/[username]/page.tsx
@@ -0,0 +1,19 @@
+import UserProfile from "@/components/UserProfile";
+import { getUserProfile } from "@/service/user";
+import { notFound } from "next/navigation";
+
+type Props = {
+  params: { username: string };
+};
+
+export default async function Userpage({ params: { username } }: Props) {
+  //상단: 사용자 프로필 이미지와 정보
+  //하단: 3개의 탭(posts, liked, bookmarks)
+  const user = await getUserProfile(username);
+
+  if (!user) {
+    notFound();
+  }
+
+  return <UserProfile user={user} />;
+}
