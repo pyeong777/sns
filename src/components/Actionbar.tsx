d2211c6443f52c9c1fa9dc50762a4f56e9364dcd
pyeong777 <seowns9520@gmail.com>
2023-10-30 13:39:54 +0900

    fix: 조건부렌더링 추가

--- src/components/Actionbar.tsx
+++ src/components/Actionbar.tsx
@@ -5,7 +5,7 @@ import { parseDate } from "@/util/date";
 type Props = {
   likes: string[];
   username: string;
-  text: string;
+  text?: string;
   createdAt: string;
 };
 
@@ -20,12 +20,14 @@ export default function Actionbar({ likes, username, text, createdAt }: Props) {
         <p className="mb-2 text-sm font-bold sm:text-base">{`좋아요 ${
           likes?.length ?? 0
         }개`}</p>
-        <p>
-          <span className="mr-1 text-sm font-bold sm:text-base">
-            {username}
-          </span>
-          <span>{text}</span>
-        </p>
+        {text && (
+          <p>
+            <span className="mr-1 text-sm font-bold sm:text-base">
+              {username}
+            </span>
+            <span>{text}</span>
+          </p>
+        )}
         <p className="my-2 text-xs text-neutral-500">
           {parseDate(createdAt, "ko")}
         </p>
