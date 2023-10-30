94a35f8f0e852c67e150ddda105198acd8530f6f
pyeong777 <seowns9520@gmail.com>
2023-10-30 15:31:29 +0900

    design: 프로필 컴포넌트 css 추가

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -14,20 +14,22 @@ export default function UserProfile({ user }: Props) {
     { title: "팔로잉", data: following },
   ];
   return (
-    <section>
-      <Avatar image={image} highlight />
-      <div>
-        <h1>{username}</h1>
-        <FollowButton user={user} />
-        <ul>
+    <section className="flex flex-col items-center justify-center w-full py-12 border-b md:flex-row border-neutral-300">
+      <Avatar image={image} highlight size="xlarge" />
+      <div className="md:ml-10 basis-1/3">
+        <div className="flex flex-col items-center md:flex-row">
+          <h1 className="my-2 text-2xl md:mr-8 md:mb-0">{username}</h1>
+          <FollowButton user={user} />
+        </div>
+        <ul className="flex gap-4 my-4">
           {info.map(({ title, data }, index) => (
             <li key={index}>
-              <span>{data}</span>
+              <span className="mr-1 font-bold">{data}</span>
               {title}
             </li>
           ))}
         </ul>
-        <p>{name}</p>
+        <p className="text-xl font-bold text-center md:text-start">{name}</p>
       </div>
     </section>
   );
