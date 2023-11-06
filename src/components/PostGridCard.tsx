14891596ede6a39304e143371a93544285813e86
pyeong777 <seowns9520@gmail.com>
2023-11-06 13:48:32 +0900

    feat: PostGrid 컴포넌트 추가(유저 정보 페이지)

---
+++ src/components/PostGridCard.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { SimplePost } from "@/model/post";
+import Image from "next/image";
+import { useState } from "react";
+import ModalPortal from "./ui/ModalPortal";
+import PostModal from "./PostModal";
+import PostDetail from "./PostDetail";
+import { signIn, useSession } from "next-auth/react";
+
+type Props = {
+  post: SimplePost;
+  priority: boolean;
+};
+
+export default function PostGridCard({ post, priority = false }: Props) {
+  const [openModal, setOpenModal] = useState(false);
+  const { image, username } = post;
+  const { data: session } = useSession();
+
+  const handleOpenPost = () => {
+    if (!session?.user) {
+      return signIn();
+    }
+    setOpenModal(true);
+  };
+
+  return (
+    <div className="relative w-full aspect-square">
+      <Image
+        onClick={handleOpenPost}
+        className="object-cover"
+        src={image}
+        alt={`photo by ${username}`}
+        fill
+        sizes="650px"
+        priority={priority}
+      />
+      {openModal && (
+        <ModalPortal>
+          <PostModal onClose={() => setOpenModal(false)}>
+            <PostDetail post={post} />
+          </PostModal>
+        </ModalPortal>
+      )}
+    </div>
+  );
+}
