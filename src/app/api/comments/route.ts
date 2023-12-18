f953eef9cdfd2c1c55e0b8d44719897cc3603acf
pyeong777 <seowns9520@gmail.com>
2023-12-18 14:05:56 +0900

    fix: 폴더 위치 변경

--- src/app/api/comments/route.ts
+++ src/app/api/comments/route.ts
@@ -1,7 +1,7 @@
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 import { addComment } from "@/service/post";
-import { authOptions } from "../[...nextauth]/route";
+import { authOptions } from "../auth/[...nextauth]/route";
 
 export async function POST(req: NextRequest) {
   const session = await getServerSession(authOptions);
